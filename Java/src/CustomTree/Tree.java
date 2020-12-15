706cac3fdaa3b65669d2a99787dce4b7196bc72b
Nikhil Raina <nxr5013@g.rit.edu>
2020-12-14 21:33:42 -0500

    Created a Custom Tree class implementing the Node class. Added Insertion with respect to BST

---
+++ Java/src/CustomTree/Tree.java
@@ -0,0 +1,50 @@
+package CustomTree;
+
+public class Tree {
+
+    private Node root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+    private Node insertNode(Node currentNode, Node node) {
+        if (currentNode == null) {
+            System.out.println("Added");
+            currentNode = node;
+        } else if (currentNode.equals(node)) {
+            System.out.println("Its equal");
+            return null;
+        } else if (currentNode.getData() < node.getData()) {
+            System.out.println("In node " + currentNode.getData());
+            currentNode.setRightNode(insertNode(currentNode.getRightNode(), node));
+        } else {
+            System.out.println("In node " + currentNode.getData());
+            currentNode.setLeftNode(insertNode(currentNode.getLeftNode(), node));
+        }
+        return currentNode;
+    }
+
+    public void insertNode(Node node) {
+        this.root = insertNode(this.root, node);
+    }
+
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        Node nodeDuplicate = new Node(4);
+        Node node78 = new Node(78);
+        tree.insertNode(node1);
+        tree.insertNode(node2);
+        tree.insertNode(node78);
+        tree.insertNode(node4);
+        tree.insertNode(nodeDuplicate);
+    }
+}
