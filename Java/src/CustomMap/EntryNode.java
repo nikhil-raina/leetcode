332fcc53ad4ff1c6a68a01ab3cbc888fd7861af1
Nikhil Raina <nxr5013@g.rit.edu>
2020-12-16 08:55:41 -0500

    Removed the EntryNode next. Things were getting a little complicated. This can be used as a better implementation though. Overrode the equals method

--- Java/src/CustomMap/EntryNode.java
+++ Java/src/CustomMap/EntryNode.java
@@ -4,26 +4,15 @@ public class EntryNode <K, V> {
     private K key;
     private V value;
 
-    private EntryNode<K, V> nextNode;
 
     public EntryNode(K key, V value) {
         this.key = key;
         this.value = value;
-        this.nextNode = null;
     }
 
     public EntryNode(K key, V value, EntryNode<K, V> nextNode) {
         this.key = key;
         this.value = value;
-        this.nextNode = nextNode;
-    }
-
-    public EntryNode<K, V> getNextNode() {
-        return nextNode;
-    }
-
-    public void setNextNode(EntryNode<K, V> nextNode) {
-        this.nextNode = nextNode;
     }
 
     public K getKey() {
@@ -36,6 +25,15 @@ public class EntryNode <K, V> {
 
     public void setKey(K key) {
         this.key = key;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof EntryNode) {
+            EntryNode<K, V> node = (EntryNode<K, V>) o;
+            return this.key.equals(node.getKey()) && this.value.equals(node.getValue());
+        }
+        return false;
     }
 
     public void setValue(V value) {
